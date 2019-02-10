a2f91a03edcb1686fc2ba9677094901fa0a3554f
rxluz <ricardo@loylap.com>
2019-02-10 18:58:34 +0530

    :memo: (Main function) Added function description and usage with commonjs and es module

--- src/lib/preventObjectRecursion.ts
+++ src/lib/preventObjectRecursion.ts
@@ -1,3 +1,35 @@
+/**
+ * Removes all recursive references in a object
+ *
+ * ### Example (es module)
+ * ```js
+ * import preventObjectRecursion from 'prevent-object-recursion'
+ *
+ * const a = { hello: 'world' }
+ * const b = { hey: 'hey', hello: a }
+ * a.newProp = b
+ *
+ * console.log(preventObjectRecursion(a))
+ * // => { hello: 'world', newProp: { hey: 'hey' }}
+ * ```
+ *
+ * ### Example (commonjs)
+ * ```js
+ * var preventObjectRecursion = require('prevent-object-recursion');
+ *
+ * const a = { hello: 'world' }
+ * const b = { hey: 'hey', hello: a }
+ * a.newProp = b
+ *
+ * console.log(preventObjectRecursion(a))
+ * // => { hello: 'world', newProp: { hey: 'hey' }}
+ * ```
+ *
+ * @param obj     The object with recursive references.
+ * @returns       Returns a new object without the recursive references.
+ * @anotherNote   Works in the browser as well as node.
+ */
+
 export default function preventObjectRecursion(obj: object): object {
   // tslint:disable-next-line
   console.log('hello world', obj);
